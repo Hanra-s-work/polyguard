ac833c5c7ffc773882e741a5cfe793d61687a0c9
Henry LETELLIER <henry.letellier@epitech.eu>
2026-03-11 18:26:08 +0100

    [ADD] the base content for the pypi module

---
+++ polyguard/__init__.py
@@ -0,0 +1,52 @@
+"""
+File in charge of easing the import of the program
+"""
+from .polyguard import TTY
+from .polyguard import HLLs
+from .polyguard import ColouriseOutput as CO
+from .polyguard import AskQuestion as AQ
+
+
+class ColouriseOutput(CO):
+    """ A rebind of the same library to help you access the ressources """
+    pass
+
+
+class AskQuestion(AQ):
+    """ A rebind of the same library to help you access the ressources """
+    pass
+
+
+class polyguard(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class polyguard(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class polyguard(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class TTYOV(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class TtyOv(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class polyguard(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class tty(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
