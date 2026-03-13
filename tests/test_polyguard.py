743a03f62a0119211527f4c1dc2d8d711492c58f
Henry LETELLIER <henry.letellier@epitech.eu>
2026-03-13 23:15:42 +0100

    [FIX] the issue in the name of the test function

--- tests/test_polyguard.py
+++ tests/test_polyguard.py
@@ -19,7 +19,7 @@
 # PROJECT: polyguard
 # FILE: test_tty_ov.py
 # CREATION DATE: 13-03-2026
-# LAST Modified: 23:6:36 13-03-2026
+# LAST Modified: 23:15:17 13-03-2026
 # DESCRIPTION:
 # A module that provides a set of swearwords to listen to when filtering while allowing to toggle on and off different languages.
 # /STOP
@@ -32,7 +32,7 @@
 from polyguard import PolyGuard
 
 
-def status_test() -> None:
+def test_status() -> None:
     error = 84
     success = 0
     instance = PolyGuard(success=success, error=error)
