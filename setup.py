7383aee31dec115dac54ca9c0cf363967851d7a6
Henry LETELLIER <henry.letellier@epitech.eu>
2026-03-13 18:03:25 +0100

    [UPDATE] the packaging files

--- setup.py
+++ setup.py
@@ -1,6 +1,34 @@
+""" 
+# +==== BEGIN polyguard =================+
+# LOGO:
+#       input
+# 
+#    @#$%!  hello
+#      |     |
+#      +--+--+
+#         |
+#         v
+#   +------------+
+#   | POLY GUARD |
+#   +------------+
+#     |        |
+#     v        v
+#  BLOCKED  PASSED
+#    KO       OK
+# /STOP
+# PROJECT: polyguard
+# FILE: setup.py
+# CREATION DATE: 13-03-2026
+# LAST Modified: 10:58:49 13-03-2026
+# DESCRIPTION:
+# A module that provides a set of swearwords to listen to when filtering while allowing to toggle on and off different languages.
+# /STOP
+# COPYRIGHT: (c) Henry Letellier
+# PURPOSE: File containing the required information to successfully build a python package
+# // AR
+# +==== END polyguard =================+
 """
-File containing the required information to successfully build a python package
-"""
+
 
 import setuptools
 
@@ -11,16 +39,10 @@ setuptools.setup(
     name='polyguard',
     version='1.0.0',
     packages=setuptools.find_packages(),
-    install_requires=[
-        "colorama==0.4.6",
-        "prettytable==3.17.0",
-        "ask_question==1.2.12",
-        "colourise-output==1.1.6",
-        "prompt-toolkit==3.0.52"
-    ],
+    install_requires=[],
     author="Henry Letellier",
     author_email="henrysoftwarehouse@protonmail.com",
-    description="A module that emulates a few core functionalities of a tty (see the inner help for a list of functions).",
+    description="A module that provides a set of swearwords to listen to when filtering while allowing to toggle on and off different languages.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Hanra-s-work/polyguard",
