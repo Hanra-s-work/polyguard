7e5e38ac56f882a286bdf01d3238159ad6beaa3b
Henry LETELLIER <henry.letellier@epitech.eu>
2026-03-13 10:46:38 +0100

    [ADD] a file so that the module would compile

---
+++ polyguard/polyguard.py
@@ -0,0 +1,41 @@
+""" 
+# +==== BEGIN polyguard =================+
+# LOGO:
+#       input
+# 
+#    @#$%!  hello
+#      |     |
+#      +--+--+
+#         |
+#         v
+#   +------------+
+#   | POLY GUARD |
+#   +------------+
+#     |        |
+#     v        v
+#  BLOCKED  PASSED
+#    KO       OK
+# /STOP
+# PROJECT: polyguard
+# FILE: polyguard.py
+# CREATION DATE: 13-03-2026
+# LAST Modified: 10:46:20 13-03-2026
+# DESCRIPTION:
+# A module that provides a set of swearwords to listen to when filtering while allowing to toggle on and off different languages.
+# /STOP
+# COPYRIGHT: (c) Henry Letellier
+# PURPOSE: This is the entry file of the module.
+# // AR
+# +==== END polyguard =================+
+"""
+
+import sys
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
