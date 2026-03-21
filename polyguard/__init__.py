613f12d683d43991d3cbbd194e658c9895ade5f4
Henry LETELLIER <henry.letellier@epitech.eu>
2026-03-21 20:10:19 +0100

    [UPDATE] the import paths to simplify module handling

--- polyguard/__init__.py
+++ polyguard/__init__.py
@@ -19,7 +19,7 @@
 # PROJECT: polyguard
 # FILE: __init__.py
 # CREATION DATE: 13-03-2026
-# LAST Modified: 23:1:32 13-03-2026
+# LAST Modified: 20:8:17 21-03-2026
 # DESCRIPTION:
 # A module that provides a set of swearwords to listen to when filtering while allowing to toggle on and off different languages.
 # /STOP
@@ -30,7 +30,9 @@
 """
 
 from .src.polyguard import PolyGuard
+from .src.constants import LangConfig
 
 __all__ = [
-    "PolyGuard"
+    "PolyGuard",
+    "LangConfig"
 ]
--- polyguard/__main__.py
+++ polyguard/__main__.py
@@ -19,7 +19,7 @@
 # PROJECT: polyguard
 # FILE: __main__.py
 # CREATION DATE: 13-03-2026
-# LAST Modified: 23:1:42 13-03-2026
+# LAST Modified: 20:9:35 21-03-2026
 # DESCRIPTION:
 # A module that provides a set of swearwords to listen to when filtering while allowing to toggle on and off different languages.
 # /STOP
@@ -29,17 +29,14 @@
 # +==== END polyguard =================+
 """
 
+import sys
 try:
-    from .src.polyguard import PolyGuard
+    from .src.cli import main
 except ImportError:
     try:
-        from src.polyguard import PolyGuard
+        from src.cli import main
     except ImportError as e:
         raise ImportError("Failed to import required files to run") from e
 
 if __name__ == "__main__":
-    ERR = 84
-    ERROR = ERR
-    SUCCESS = 0
-    PI = PolyGuard()
-    PI.main()
+    sys.exit(main(sys.argv))
